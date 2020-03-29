79d413d267cc1b65cf716e04d2f034c4458f0ca4
Daniel Chipolesch <danielchipolesch@gmail.com>
2020-03-29 15:59:57 -0300

    usuario x cpf 1) Agora o login não é mais o usuário, mas sim o CPF do militar; 2) Eliminação de código inútil.

--- login.php
+++ login.php
@@ -2,18 +2,6 @@
 session_start();
 include('conexao.php');
 
-//CANCELAR ORÇAMENTOS APÓS 5 DIAS
-
-$query = "SELECT * FROM orcamentos WHERE status = 'Aguardando'";
-$result = mysqli_query($conexao, $query);
-while ($res_1 = mysqli_fetch_array($result)) {
-	$data_geracao = $res_1['data_geracao'];
-	$data_cancelamento = date('Y/m/d', strtotime("-7 days", strtotime(date('Y/m/d'))));
-	$query_editar = "UPDATE orcamentos SET status = 'Cancelado' WHERE data_geracao = '$data_cancelamento' AND status = 'Aguardando'";
-	$result_editar = mysqli_query($conexao, $query_editar);
-}
-
-
 if (empty($_POST['usuario']) || empty($_POST['senha'])) {
 	header('Location: index.php');
 	exit();
@@ -21,7 +9,7 @@ if (empty($_POST['usuario']) || empty($_POST['senha'])) {
 
 $usuario = mysqli_real_escape_string($conexao, $_POST['usuario']);
 $senha = mysqli_real_escape_string($conexao, md5($_POST['senha']));
-$query = "SELECT * FROM usuarios WHERE usuario = '{$usuario}' AND senha = '{$senha}'";
+$query = "SELECT * FROM militares WHERE cpf = '{$usuario}' AND senha = '{$senha}'";
 $result = mysqli_query($conexao, $query);
 $dado = mysqli_fetch_array($result);
 $row = mysqli_num_rows($result);
@@ -32,7 +20,7 @@ if ($row > 0) {
 	$_SESSION['perfil_usuario'] = $dado['perfil'];
 
 
-	if ($_SESSION['perfil_usuario'] == 'Administrador' || $_SESSION['perfil_usuario'] == 'Gerente') {
+	if ($_SESSION['perfil_usuario'] == 'ADMIN' || $_SESSION['perfil_usuario'] == 'Gerente') {
 		header('Location: apps/admin/painel_admin.php');
 		exit();
 	}
@@ -44,11 +32,6 @@ if ($row > 0) {
 
 	if ($_SESSION['perfil_usuario'] == 'EXANT') {
 		header('Location: apps/exercicioanterior/exant/painel_exant.php');
-		exit();
-	}
-
-	if ($_SESSION['perfil_usuario'] == 'Funcionário') {
-		header('Location: painel_funcionario.php');
 		exit();
 	}
 
