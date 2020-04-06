9b8324fa312342fc23c80c3ae8a0270c20f0eb02
Daniel Chipolesch <danielchipolesch@gmail.com>
2020-04-06 01:24:38 -0300

    Correção de algoritmo 1) Quando a sessão é revogada, o usuário era redirecionado para o index.php errado. Agora, esse erro foi corrigido.

--- verificar_login.php
+++ verificar_login.php
@@ -1,13 +1,12 @@
 <?php
-if (!$_SESSION['usuario']) {
-	header('Location: index.php');
-	exit();
-}
-
 function login($a, $b)
 {
 	if ($_SESSION['perfil_usuario'] != $a) {
-		header('Location: ' . $b . 'index.php');
+		header('Location:' . $b . 'index.php');
+		exit();
+	}
+	if (!$_SESSION['usuario']) {
+		header('Location:' . $b . 'index.php');
 		exit();
 	}
 }
