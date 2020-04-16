c4895483ca0b1e78a1a3efd76fa94c4d099c459b
Daniel Chipolesch <danielchipolesch@gmail.com>
2020-04-16 01:01:17 -0300

    Inclusão do Toast 1) Foi incluído o Toast caso o usuário não seja autenticado.

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include('dist/php/functions.php');
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,12 +9,15 @@ session_start();
   <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
   <link href="dist/css/index.css" rel="stylesheet" id="bootstrap-css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="plugins/toastr/toastr.css" />
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
   <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <script src="plugins/sweetalert2/sweetalert2.all.min.js"></script>
   <script src="plugins/jquery-mask/dist/jquery.mask.js"></script>
+  <script src="plugins/toastr/toastr.min.js"></script>
   <title>SISPAGPES</title>
 </head>
 
@@ -41,10 +45,10 @@ session_start();
           <div class="invalid-feedback">Please fill out this field.</div>
         </div>
         <?php
-        if (isset($_SESSION['nao_autenticado'])) : ?>
-          <p class="fadeIn fourth"><small>Usuário ou Senha Inválidos!</small></p>
-        <?php
-        endif;
+        if (isset($_SESSION['nao_autenticado'])) {
+          //AlertaConsulta('error', 'ERRO', 'CPF ou SENHA inválidos');        
+          Toast('error', 'CPF ou SENHA inválidos!');
+        }
         unset($_SESSION['nao_autenticado']);
         ?>
         <div style="padding-bottom:15px;">
