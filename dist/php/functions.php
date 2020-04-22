a5d8aa8f15e6498814e7ec961168732cd11c3daf
Daniel Chipolesch <danielchipolesch@gmail.com>
2020-04-22 02:11:20 -0300

    Function head($a)
    1) Foi adicionada a function($a) que contém a tag <head> de modo a centralizar todas as heads na página functions.php
    2) Isso deixou o carregamendo das páginas um pouco mais lento. Achar uma forma de otimizar isso.

--- dist/php/functions.php
+++ dist/php/functions.php
@@ -1,4 +1,49 @@
 <?php
+function head($a)
+{
+  echo '
+  <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="icon" type="image/png" href="' . $a . 'dist/img/gapls.png">
+	<title>SISPAGPES</title>
+	<!-- Tell the browser to be responsive to screen width -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="' . $a . 'plugins/fontawesome-free/css/all.min.css">
+	<!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Select2 -->
+  <link rel="stylesheet" href="' . $a . 'plugins/select2/css/select2.min.css">
+  <link rel="stylesheet" href="' . $a . 'plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+	<!-- Tempusdominus Bbootstrap 4 -->
+	<link rel="stylesheet" href="' . $a . 'plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+	<!-- iCheck -->
+	<link rel="stylesheet" href="' . $a . 'plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+	<!-- JQVMap -->
+	<link rel="stylesheet" href="' . $a . 'plugins/jqvmap/jqvmap.min.css">
+	<!-- Theme style -->
+	<link rel="stylesheet" href="' . $a . 'dist/css/adminlte.min.css">
+	<link rel="stylesheet" href="' . $a . 'dist/css/style_print_button.css">
+	<!-- overlayScrollbars -->
+	<link rel="stylesheet" href="' . $a . 'plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+	<!-- Daterange picker -->
+	<link rel="stylesheet" href="' . $a . 'plugins/daterangepicker/daterangepicker.css">
+	<!-- JQuery UI -->
+	<link rel="stylesheet" href="' . $a . 'plugins/jquery-ui/jquery-ui.min.css">
+	<!-- summernote -->
+	<link rel="stylesheet" href="' . $a . 'plugins/summernote/summernote-bs4.css">
+	<!-- Google Font: Source Sans Pro -->
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+	<!-- Material Design-->
+	<link href="https://unpkg.com/material-components-web@v4.0.0/dist/material-components-web.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<!-- SweetAlert2 -->
+	<script src="' . $a . 'plugins/sweetalert2/sweetalert2.min.js"></script>
+	<script src="' . $a . 'plugins/sweetalert2/sweetalert2.all.min.js"></script>
+	<!-- Toastr -->
+  <script src="' . $a . 'plugins/toastr/toastr.min.js"></script>';
+}
+
 function footer()
 {
   echo '<strong>&copy; 2019-' . date("Y") . '<a href="#"> SISPAGPES</a></strong>. Desenvolvido por DANIEL ANGELO CHIPOLESCH DE ALMEIDA 1º Ten Int. <div class="float-right d-none d-sm-inline-block"><b>Versão</b> 1.0.0</div>';
