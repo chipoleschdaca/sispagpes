198d61e6bf32903e3d22d6b776b5a4550818502b
Daniel Chipolesch <danielchipolesch@gmail.com>
2020-04-29 01:41:44 -0300

    UTF8 decoder

--- conexao.php
+++ conexao.php
@@ -6,4 +6,5 @@ define('SENHA', '123456');
 define('BD', 'sispagpes');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, BD) or die('Não foi possível conectar!');
+mysqli_set_charset($conexao, 'utf8');
 $url = "http://localhost/sispagpes";
