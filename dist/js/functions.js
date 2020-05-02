ecb015834b5423ac8f0cd7ea862df2bb8b43903b
Daniel Chipolesch <danielchipolesch@gmail.com>
2020-05-02 03:05:41 -0300

    Funções para uso futuro

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -9,3 +9,66 @@ function Alerta(tipo, titulo, msg, location) {
     window.location = 'location';
   }, 2000);
 }
+
+function AlertaConsulta(tipo, titulo, msg) {
+
+  Swal.fire({
+    type: 'tipo',
+    title: 'titulo',
+    text: 'msg',
+    showConfirmButton: false,
+    timer: 2000
+  });
+
+}
+
+function AlertaExcluir(tipo, titulo, msg, location) {
+
+  Swal.fire({
+    title: 'titulo',
+    text: 'msg',
+    icon: 'tipo',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete it!'
+  }).then((result) => {
+      if (result.value) {
+        Swal.fire(
+          'Deleted!',
+          'Your file has been deleted.',
+          'success'
+        );
+      }
+    },
+    window.location = 'location'
+  )
+}
+
+function AlertaLocation(tipo, titulo, msg, location) {
+  Swal.fire({
+      type: 'tipo',
+      title: 'titulo',
+      text: 'msg',
+      showConfirmButton: false,
+      timer: 2000
+    },
+    function () {
+      window.location.href = 'location';
+    });
+}
+
+function Toast(tipo, titulo) {
+  $(function () {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000
+    });
+    Toast.fire({
+      type: 'tipo',
+      title: 'titulo'
+    });
+  });
+}
