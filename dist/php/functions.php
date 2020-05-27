34f59f8e48d4d4408e03e9cc864fa47fa7cbca59
Daniel Chipolesch <danielchipolesch@gmail.com>
2020-05-27 11:02:39 -0300

    Atualização para a v1.0.1 do sistema.

--- dist/php/functions.php
+++ dist/php/functions.php
@@ -50,7 +50,7 @@ function head($diretorio)
 
 function footer()
 { ?>
-  <span>&copy; 2019-<?php echo date("Y") ?> <a href="#"> SISPAGPES</a></span> - Desenvolvido por DANIEL ANGELO CHIPOLESCH DE ALMEIDA 1º Ten Int <div class="float-right d-none d-sm-inline-block">v1.0.0</div>
+  <span>&copy; 2019-<?php echo date("Y") ?> <a href="#"> SISPAGPES</a></span> - Desenvolvido por DANIEL ANGELO CHIPOLESCH DE ALMEIDA 1º Ten Int <div class="float-right d-none d-sm-inline-block">v1.0.1</div>
 <?php
 }
 
