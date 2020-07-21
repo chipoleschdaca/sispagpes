77c259cd255e18d4881fa2f330b2e58f83f4ae94
Daniel Chipolesch <danielchipolesch@gmail.com>
2020-07-20 23:14:38 -0300

    Release 2.0.0
    Excluídos todos os arquivos da versão anterior e está sendo desenvolvida a versão 2.0.0 do SISPAGPES.

--- dist/php/pageFooter.php
+++
@@ -1,3 +0,0 @@
-<footer class="main-footer" style="font-size: 12px;">
-  <span>&copy; 2019-<?= date("Y") ?><a href="#" style="margin-right: 0;"><b> SISPAGPES</b></a></span>. Desenvolvido por DANIEL ANGELO <span style="text-decoration: underline;"><b>CHIPOLESCH</b></span> DE ALMEIDA <b>1º Ten Int</b>.<div class="float-right d-none d-sm-inline-block">v1.0.2</div>
-</footer>
